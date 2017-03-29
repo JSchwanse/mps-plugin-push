7e88959fb2d188d586cf728c1f4ef8e2222d26fc
JSWA <j.schwanse@mps-solutions.de>
2017-03-29 11:34:10 +0200

    Use fs correctly

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -3,17 +3,16 @@ module.exports = function(context) {
     var path = require('path');
     var platformProjPath = path.join(context.opts.projectRoot, 'platforms/windows');
 
-    const fs = require('fs');
-    fs.readdirSync(context.opts.projectRoot, function(err, files){
-        files.forEach(function(file){
-            console.log(file);
-        });
+    var fs = require('fs');
+    var files = fs.readdirSync(path.resolve(context.opts.projectRoot));
+    files.forEach(function(file){
+        console.log(file);
     });
 
-    var AppxManifest = require('./AppxManifest.js');
-    ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
-        var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
-        manifest.getVisualElements().setToastCapable(true);
-        manifest.write();
-    });
+    //var AppxManifest = require('./AppxManifest.js');
+    //['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
+    //    var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
+    //    manifest.getVisualElements().setToastCapable(true);
+    //    manifest.write();
+    //});
 }
