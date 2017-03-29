5fc5f137375f00a67d50875c65ccb70cfa49cda1
JSWA <j.schwanse@mps-solutions.de>
2017-03-29 11:17:59 +0200

    Try a relative path

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -2,7 +2,7 @@ module.exports = function(context) {
     console.log('Updating appxmanifests with ToastCapable=true...');
     var path = require('path');
     var platformProjPath = path.join(context.opts.projectRoot, 'platforms/windows');
-    var AppxManifest = require(path.resolve(__dirName, './AppxManifest.js'));
+    var AppxManifest = require('./AppxManifest.js'));
 
     ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
         var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
