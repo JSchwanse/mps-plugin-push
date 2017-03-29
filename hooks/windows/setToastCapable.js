68ba121f84f7b46f9ddef9955e7e3e52d87e3505
JSWA <j.schwanse@mps-solutions.de>
2017-03-29 11:42:09 +0200

    Fix pathing for winows hook

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -1,18 +1,12 @@
 module.exports = function(context) {
     console.log('Updating appxmanifests with ToastCapable=true...');
     var path = require('path');
-    var platformProjPath = path.join(context.opts.projectRoot, 'platforms/windows');
+    var platformProjPath = path.resolve(context.opts.projectRoot);
 
-    var fs = require('fs');
-    var files = fs.readdirSync(path.resolve(context.opts.projectRoot));
-    files.forEach(function(file){
-        console.log(file);
+    var AppxManifest = require(path.resolve(platformProjPath, './corodova/lib/AppxManifest.js'));
+    ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
+        var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
+        manifest.getVisualElements().setToastCapable(true);
+        manifest.write();
     });
-
-    //var AppxManifest = require('./AppxManifest.js');
-    //['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
-    //    var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
-    //    manifest.getVisualElements().setToastCapable(true);
-    //    manifest.write();
-    //});
 }
