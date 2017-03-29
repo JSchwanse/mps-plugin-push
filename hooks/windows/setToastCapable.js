0046f06bc370910428961c59f19dbf5fac1d57b1
JSWA <j.schwanse@mps-solutions.de>
2017-03-29 11:24:19 +0200

    Check available files

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -2,8 +2,15 @@ module.exports = function(context) {
     console.log('Updating appxmanifests with ToastCapable=true...');
     var path = require('path');
     var platformProjPath = path.join(context.opts.projectRoot, 'platforms/windows');
-    var AppxManifest = require('./AppxManifest.js');
 
+    const fs = require('fs');
+    fs.readdirSync(context.opts.projectRoot, (err, files) => {
+        files.forEach(file => {
+            console.log(file);
+        });
+    });
+
+    var AppxManifest = require('./AppxManifest.js');
     ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
         var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
         manifest.getVisualElements().setToastCapable(true);
