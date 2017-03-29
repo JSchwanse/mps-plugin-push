f1bf2750f5bede2baa46843e29abd97c979b84c5
JSWA <j.schwanse@mps-solutions.de>
2017-03-29 13:11:26 +0200

    Fixed two ')' where not needed.

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -6,9 +6,9 @@ module.exports = function(context) {
     var platformProjPath = path.resolve(context.opts.projectRoot);
     var manifestPath;
     if (fs.existsSync(path.resolve(platformProjPath, './cordova/lib/AppxManifest.js'))){
-        manifestPath = path.resolve(platformProjPath, './cordova/lib/AppxManifest.js'));
+        manifestPath = path.resolve(platformProjPath, './cordova/lib/AppxManifest.js');
     } else {
-        manifestPath = path.resolve(platformProjPath, './platforms/windows/cordova/lib/AppxManifest.js'));
+        manifestPath = path.resolve(platformProjPath, './platforms/windows/cordova/lib/AppxManifest.js');
     }
 
     var AppxManifest = require(manifestPath);
