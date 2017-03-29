69e938b1ea5cf829b566133d801b40a18cff8439
JSWA <j.schwanse@mps-solutions.de>
2017-03-29 13:09:27 +0200

    Compatibility between local build and PGB

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -1,9 +1,17 @@
 module.exports = function(context) {
     console.log('Updating appxmanifests with ToastCapable=true...');
     var path = require('path');
-    var platformProjPath = path.resolve(context.opts.projectRoot);
+    var fs = require('fs');
 
-    var AppxManifest = require(path.resolve(platformProjPath, './cordova/lib/AppxManifest.js'));
+    var platformProjPath = path.resolve(context.opts.projectRoot);
+    var manifestPath;
+    if (fs.existsSync(path.resolve(platformProjPath, './cordova/lib/AppxManifest.js'))){
+        manifestPath = path.resolve(platformProjPath, './cordova/lib/AppxManifest.js'));
+    } else {
+        manifestPath = path.resolve(platformProjPath, './platforms/windows/cordova/lib/AppxManifest.js'));
+    }
+
+    var AppxManifest = require(manifestPath);
     ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
         var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
         manifest.getVisualElements().setToastCapable(true);
